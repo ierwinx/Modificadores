09a4e2f85618375d97ccb2d0d68bfdb8212ab6df
Eяw!и Gontier <ierwinx@me.com>
2022-08-01 23:10:14 -0500

    Initial Commit

---
+++ Modificadores/ModificadoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModificadoresApp.swift
+//  Modificadores
+//
+//  Created by Erwin Luz León on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ModificadoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
